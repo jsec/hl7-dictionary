1e474e88e54842c9b865215a6b574c92b8e8e3d6
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-04-08 16:35:55 -0400

    add tsconfig and initial utils skeleton

---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+function getSegmentDefinition(segmentName: string, version: string): SegmentDefinition {
+    return null;
+}
+
+function getFieldDefinition(fieldName: string, version: string): FieldDefinition {
+    return null;
+}
+
+function getMessageDefinition(messageType: string, version: string): MessageDefinition {
+    return null;
+}
